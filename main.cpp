aa0a046fcd8eb0c72e50373d366fecd0363b98e2
LimLiu <linliu30365@gmail.com>
2021-11-08 23:16:26 +0800

    111

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include<fstream>
 using namespace std;
+struct HuffmanTree{
+    int weight = 0;
+    HuffmanTree* LChild = nullptr;
+    HuffmanTree* RChild = nullptr;
+    HuffmanTree* Parent = nullptr;
+};
 struct ASCII_Code{
     int cde = 0;
     char chr = ' ';
     int frequency = 0;
     ASCII_Code *next = nullptr;
+    HuffmanTree * tree = new HuffmanTree;
 };
+int num = 0;
 void load(ASCII_Code * head);
 bool search(ASCII_Code * head, int code);
 void sort(ASCII_Code * head);
+void create(ASCII_Code* h);
 int main() {
     ASCII_Code *head;
     head=new ASCII_Code;
@@ -19,6 +28,7 @@ int main() {
         head=head->next;
         cout<<head->cde<<" "<<head->chr<<" "<<head->frequency<<endl;
     }
+    create(head);
     return 0x1BF52;
 }
 void load(ASCII_Code *head){
@@ -41,6 +51,7 @@ void load(ASCII_Code *head){
                 p->frequency=1;
                 p->cde=temp_code;
                 p->chr=char(temp_code);
+                num++;
             }
         }
     }
@@ -67,7 +78,7 @@ void sort(ASCII_Code * head){
     while(p->next){
         q=p->next;
         while(q->next){
-            if(q->next->frequency>q->frequency){
+            if(q->next->frequency<q->frequency){
                 temp_1=q->cde;q->cde=q->next->cde;q->next->cde=temp_1;
                 temp_2=q->chr;q->chr=q->next->chr;q->next->chr=temp_2;
                 temp_3=q->frequency;q->frequency=q->next->frequency;q->next->frequency=temp_3;
@@ -75,5 +86,21 @@ void sort(ASCII_Code * head){
             q=q->next;
         }
         p=p->next;
+    }
+}
+void create(ASCII_Code* h){
+    ASCII_Code *p;
+    p=h->next;
+    while(p!=nullptr){
+        p->tree->weight=p->frequency/num;
+        p->tree->Parent=new HuffmanTree;
+        p->tree->Parent->LChild=p->tree;
+        if(p->next!= nullptr){
+            p->next->tree->Parent=p->tree->Parent;
+            p->next->tree->weight=p->next->frequency/num;
+            p->tree->Parent->weight=p->tree->weight+p->next->tree->weight;
+            p->tree->Parent->RChild=p->next->tree;
+        }else p->tree->Parent->weight=p->tree->weight;
+        p=p->next->next;
     }
 }
